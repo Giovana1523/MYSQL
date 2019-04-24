b0bb816ea0695af787e8c65c593f3c25de45c6d6
Giovana1523 <40663190+Giovana1523@users.noreply.github.com>
2019-04-24 08:47:13 -0300

    MYSQL

---
+++ script.sql
@@ -0,0 +1,21 @@
+CREATE TABLE cliente(
+	cpf varchar(11) AUTO_INCREMENT,
+	nome varchar(30) NOT NULL,
+	endereco varchar(50),
+	PRIMARY KEY (cpf)
+);
+
+CREATE TABLE produto(
+	codigo int(8) AUTO_INCREMENT,
+	descricao varchar(50),
+	preco float(100) NOT NULL,
+	unidade varchar(5),
+	PRIMARY KEY(codigo)
+);
+CREATE TABLE compra(
+	cpf_cliente varchar(11),
+	codigo_produto int(8),
+	quantidade float NOT NULL,
+	FOREIGN KEY (cpf_cliente) REFERENCES cliente (cpf),
+	FOREIGN KEY (codigo_produto) REFERENCES produto (codigo)
+);
